1962e74e0da02bf63bccf3b2b8d823762eefd473
MarxSoul55 <wjacademic@gmail.com>
2017-05-15 16:01:15 -0700

    Created final source and executable.

---
+++ README.md
@@ -0,0 +1,10 @@
+### Disclaimer
+The following code is, by the nature of the project's requirements, broken. Don't use it unless you
+desire a broken calculator.
+### Description
+This repository contains code for a school project for the Computer Science 41 class. The project's 
+folder `legacy` contains contextual information for the project, as well as the original source
+files, with `MAIN.cc` being the entry-point.
+
+The final version of the code is in `src`, with the corresponding compiled program (for Windows) 
+available under `a.exe`.
---
+++ src/main.cc
@@ -0,0 +1,251 @@
+// By:  Beltran, Jow, Machado, Gardner, Borden
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <sstream>
+#include <cctype>
+#include <cmath>
+#include <stack>
+#include <cstdlib>
+
+using namespace std;
+
+// Pre-Con: Some sort of unacceptable error has occurred that warrants a shutdown.
+// Post-Con: The program dies, printing "INVALID INPUT!"
+void die() {
+    cout << "INVALID INPUT!" << endl;
+    exit(EXIT_FAILURE);
+}
+
+// Pre-Con: Reached end of a file.
+// Post-Con: Peaceful exit, no "INVALID INPUT!"
+void peacefully_die() {
+    // Die peacefully, no "INVALID INPUT!"
+    exit(EXIT_SUCCESS);
+}
+
+// Pre-Con: Need some way to check if string is integer.
+// Post-Con: Function returns true if so, false otherwise
+// Input: String to check
+inline bool is_int(const string &s) {
+    if (s.empty() || ((!isdigit(s[0])) && (s[0] != '+'))) return false;
+    char *p;
+    strtol(s.c_str(), &p, 10);
+    return (*p == 0);
+}
+
+
+class SpecialInt {
+    private:
+        int data;
+    public:
+        // Pre-Con: Trying to set a variable.
+        // Post-Con: Set the variable, die if invalid.
+        // Input: Integer to set.
+        void set_data(int x) {
+            if (x > 255) die();
+            data = x;
+        }
+        // Pre-Con: Trying to get variable.
+        // Post-Con: Returns variable.
+        int get_data() {
+            if (data > 255) die();
+            return data;
+        }
+        // Pre-Con: Need to increment data by 1 per directions.
+        // Post-Con: Done, but error if now > 255.
+        void increment_data() {
+            if (data + 1 > 255) die();
+            data++;
+        }
+};
+
+// Hash table.
+unordered_map <string, SpecialInt> table;
+
+// Pre-Con: Need to read input from user.
+// Post-Con: Done, but error if invalid statement or variable set to negative number.
+vector<string> input() {
+    vector<string> retval;
+    string statement;
+    getline(cin, statement);
+    if (!cin) peacefully_die();
+    size_t found_let = statement.find("LET");
+    size_t found_equals = statement.find("=");
+    // "LET" is in there, so we're setting a variable!
+    if (found_let != string::npos) {
+        // But wait! Remember, a variable can only be one letter.
+        // So how can we check for that? Well, if the variable is only one letter...
+        // Then "=" will always be at the same index: 6
+        if (found_equals == 6) {
+            // Okay, so by now we know everything from the beginning to "=" is fine!
+            // You may ask, what about the number? What if it's out of bounds?
+            // Well, the SpecialInt class will handle that :)
+            string var_name;
+            var_name.push_back(statement.at(4));
+            // Value should start at index: 8
+            // But wait! No negatives.
+            if (statement.at(8) == '-') die();
+            string var_value = statement.substr(8, (statement.size() - 1) - 7);
+            retval.push_back(var_name);
+            retval.push_back(var_value);
+            return retval;
+        }
+        // OK, it's an error!
+        else {
+            die();
+        }
+    }
+    // So there's no "LET", but maybe it's just an expression? An expression wouldn't have "=".
+    else if (found_equals == string::npos) {
+        retval.push_back(statement);
+        return retval;
+    }
+    // OK, it's an error for sure!
+    else {
+        die();
+    }
+}
+
+// Pre-Con: Need logic of calculator.
+// Post-Con: Result, but error if trying to use nonexistent variable.
+// Input: statement, e.g. "x + y / 2"
+int logic(string statement) {
+    int retval;
+    // Separate the statement into logical parts.
+    istringstream iss(statement);
+    vector<string> vec;
+    copy(istream_iterator<string> (iss), istream_iterator<string>(), back_inserter(vec));
+    // No operators at the end of vec! Makes no sense!
+    string last = vec.at(vec.size()-1);
+    if (last == "+" || last == "-" || last == "*" || last == "/" || last == "^" || last == "%") {
+        die();
+    }
+    // Create stack for evaluation.
+    vector<string> op_table;
+    /*
+    if (vec.size() == 1) {
+        //
+    }
+    */
+    for (string s : vec) {
+        // We're going to treat this kinda like the RPN calculator.
+        op_table.push_back(s);
+        // This fucks all the logic up! Needed to add check for this!
+        if (vec.size() == 1) {
+            if (table.find(s) != table.end()) {
+                retval = table[s].get_data();
+                table[s].increment_data();
+                //retval = table[s].get_data();
+                return retval;
+            } else if (is_int(s)) {
+                retval = stoi(s);
+                return retval;
+            } else { die(); } // Doesn't exist.
+        }
+        if (op_table.size() == 3) {
+            // We got the operator.
+            string operat = op_table.at(1);
+            // We have to declare the first and second values because of stupid C++ scoping rules.
+            int val1;
+            int val2;
+            // The first value.
+            // If the string is not an integer, it must be a variable. Look it up!
+            // We're also handling conversion from a string to an integer with this.
+            if (!is_int(op_table.at(0))) {
+                if (table.find(op_table.at(0)) == table.end()) die();
+                val1 = table[op_table.at(0)].get_data();
+                table[op_table.at(0)].increment_data();
+            }
+            // OK, it's an integer then. Let's make it so (via conversion as before).
+            else { val1 = stoi(op_table.at(0)); }
+            // Do the same process for the second item.
+            if (!is_int(op_table.at(2))) {
+                if (table.find(op_table.at(2)) == table.end()) die();
+                val2 = table[op_table.at(2)].get_data();
+                table[op_table.at(2)].increment_data();
+            }
+            else { val2 = stoi(op_table.at(2)); }
+            // Now we got the integer values. Now based off of the operator, let's do this!
+            if (operat == "+") {
+                // We got the result.
+                int result = val1 + val2;
+                // Clear the operation table.
+                op_table.clear();
+                // Now add the string of the result back onto it!
+                op_table.push_back(to_string(result));
+                retval = result;
+            }
+            else if (operat == "-") {
+                int result = val1 - val2;
+                op_table.clear();
+                op_table.push_back(to_string(result));
+                retval = result;
+            }
+            else if (operat == "*") {
+                int result = val1 * val2;
+                op_table.clear();
+                op_table.push_back(to_string(result));
+                retval = result;
+            }
+            else if (operat == "/") {
+                if (val2 == 0) die();
+                int result = val1 / val2;
+                op_table.clear();
+                op_table.push_back(to_string(result));
+                retval = result;
+            }
+            else if (operat == "^") {
+                // No 0^0
+                if (val1 == 0 && val2 == 0) die();
+                int result = pow(val1, val2);
+                op_table.clear();
+                op_table.push_back(to_string(result));
+                retval = result;
+            }
+            else if (operat == "%") {
+                // No 0 % 0
+                if (val2 == 0) die();
+                int result = val1 % val2;
+                op_table.clear();
+                op_table.push_back(to_string(result));
+                retval = result;
+            }
+        }
+    }
+    return retval;
+}
+
+// Pre-Con: Main function for operation.
+// Post-Con: Main function, quits when necessary.
+int main() {
+    // Hash table that holds variables.
+    unordered_map<string, SpecialInt> hash;
+    while (true) {
+        // Get the input-vector from the function.
+        vector<string> vec = input();
+        // It's setting a variable.
+        if (vec.size() == 2) {
+            SpecialInt val;
+            val.set_data(stoi(vec.at(1)));
+            // But wait! We can't redeclare a variable! Let's make sure of that.
+            if (table.find(vec.at(0)) == table.end()) {
+                table[vec.at(0)] = val;
+            }
+            // We must be trying to redeclare a variable! Bad!
+            else {
+                die();
+            }
+        }
+        else {
+            // Call logic function.
+            int result = logic(vec.at(0));
+            cout << result << endl;
+        }
+    }
+    return 0;
+}
